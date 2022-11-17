bfb568e295790034e1f1713dad012efab1116292
kent vejrup madsen <kent.vejrup.madsen@outlook.com>
2022-11-17 10:36:34 +0100

    current

--- code/README.md
+++ code/README.md
@@ -15,10 +15,5 @@ npm run serve
 npm run build
 ```
 
-### Run your unit tests
-```
-npm run test:unit
-```
-
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- code/src/components/HelloWorld.vue
+++ code/src/components/HelloWorld.vue
@@ -12,7 +12,6 @@
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
     </ul>
     <h3>Essential Links</h3>
     <ul>
